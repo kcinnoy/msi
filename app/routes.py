24e7a395d92c8e5b938bcef84133705c1a197309
kcinnoy <yonnick00@gmail.com>
2020-08-24 00:00:51 +0100

    request login feature added

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'YB'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -19,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so coolio!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,7 +33,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 	
 @app.route('/logout')
