c16346d52397fd50cfa155b296ef685e3bd821ee
kcinnoy <yonnick00@gmail.com>
2020-08-26 10:13:06 +0100

    added explore page, routr and updated base, indeex html pages

--- app/routes.py
+++ app/routes.py
@@ -23,17 +23,15 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so coolio!'
-        }
-    ]
-    return render_template('index.html', title='Home', form=form, posts=posts)
+    
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form,posts=posts)
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
