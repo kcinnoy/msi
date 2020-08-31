2e91b81dffb7c8b4524cf63db83bff772eb83a38
kcinnoy <yonnick00@gmail.com>
2020-08-31 18:35:40 +0100

    added metric model

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    metrics = db.relationship('Metric', backref='creator', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
@@ -68,3 +69,30 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Metric(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(120), index=True)
+    service_element_name = db.Column(db.String(120), index=True)
+    service_level_detail = db.Column(db.Text,index=False)
+    target = db.Column(db.Text, index=True)
+    service_provider_steward_1 = db.Column(db.String(120), index=True)
+    metric_name = db.Column(db.String(120), index=True)
+    metric_description = db.Column(db.Text,index=False )
+    metric_rationale = db.Column(db.Text, index=False)
+    metric_value_display_format = db.Column(db.String(120), index=False)
+    threshold_target = db.Column(db.Integer, index=False)
+    threshold_target_rationale = db.Column(db.String(500), index=False)
+    threshold_target_direction = db.Column(db.String(10), index=False)
+    threshold_trigger = db.Column(db.Integer, index=False)
+    threshold_trigger_rationale = db.Column(db.String(500), index=False)
+    threshold_trigger_direction = db.Column(db.String(10), index=False)
+    data_source = db.Column(db.String(120), index=True)
+    data_update_frequency = db.Column(db.String(120), index=True)
+    metric_owner_primary = db.Column(db.String(120), index=True)
+    vantage_control_id = db.Column(db.String(120), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Metric {}>'.format(self.service_name)
